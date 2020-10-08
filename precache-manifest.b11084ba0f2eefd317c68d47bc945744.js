c1bc146fec11350adc477b8477f3ce11f94e7388
Gregg Tavares (via Travis CI) <github@greggman.com>
2020-10-08 07:54:36 +0000

    Deploy greggman/jsbenchit to github.com/greggman/jsbenchit.git:gh-pages

---
+++ precache-manifest.b11084ba0f2eefd317c68d47bc945744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210183bc97fafe48cb25e8570a931be2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cf9c5a0cf0e3375fe41",
+    "url": "/static/css/2.ca0d49c0.chunk.css"
+  },
+  {
+    "revision": "36243b29c4d8765a8d8b",
+    "url": "/static/css/main.2244ce29.chunk.css"
+  },
+  {
+    "revision": "1cf9c5a0cf0e3375fe41",
+    "url": "/static/js/2.d60e469d.chunk.js"
+  },
+  {
+    "revision": "0272c1b36924fa71c4b7895d88664c01",
+    "url": "/static/js/2.d60e469d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36243b29c4d8765a8d8b",
+    "url": "/static/js/main.576d86ce.chunk.js"
+  },
+  {
+    "revision": "661bdcab10ba56788d88",
+    "url": "/static/js/runtime-main.0cac3f20.js"
+  }
+]);
