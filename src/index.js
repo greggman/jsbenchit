b4f398e61ab6bbaac00800c93e7e630b894d99f8
Gregg Tavares <github@greggman.com>
2020-10-08 16:58:32 +0900

    local only

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (window.location.hostname.startsWith('localhost')) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+} else {
+  document.body.innerHTML = '<h1>coming soon...</h1>';
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
