b8df43262aca9c210324c6f75727bcf6c58e900b
Gregg Tavares (via Travis CI) <github@greggman.com>
2020-10-24 04:39:33 +0000

    Deploy greggman/jsbenchit to github.com/greggman/jsbenchit.git:gh-pages

--- precache-manifest.359e22164c782179c4cfcb50075c6144.js
+++ precache-manifest.359e22164c782179c4cfcb50075c6144.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "50fbfd3c808ae09c1259a12b569dc3bc",
+    "revision": "491422877fbbb7eaaa42bd96f10e6956",
     "url": "/index.html"
   },
   {
-    "revision": "a45f033339ca0210961c",
+    "revision": "8aa0156e500b3ea0125f",
     "url": "/static/css/2.ca0d49c0.chunk.css"
   },
   {
-    "revision": "e5b88ae532c5690c2e52",
+    "revision": "4550cb617c597a3d345e",
     "url": "/static/css/main.e5528caa.chunk.css"
   },
   {
-    "revision": "a45f033339ca0210961c",
-    "url": "/static/js/2.3e066b4f.chunk.js"
+    "revision": "8aa0156e500b3ea0125f",
+    "url": "/static/js/2.cae21d84.chunk.js"
   },
   {
     "revision": "991bfe13d76d3486a0f57d532df393c5",
-    "url": "/static/js/2.3e066b4f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.cae21d84.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e5b88ae532c5690c2e52",
-    "url": "/static/js/main.6af8d73d.chunk.js"
+    "revision": "4550cb617c597a3d345e",
+    "url": "/static/js/main.6eb52d90.chunk.js"
   },
   {
     "revision": "661bdcab10ba56788d88",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48ecf7b71689efc5a32a18cbefb1a0bb.js"
+  "/precache-manifest.359e22164c782179c4cfcb50075c6144.js"
 );
 
 self.addEventListener('message', (event) => {
